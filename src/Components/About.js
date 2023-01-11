8e8fda85c4a78b1fedfe4b8c8205d7531dccf754
Muhammed Rashid <rashidktr07@gmail.com>
2023-01-11 15:19:50 +0530

    Completee about page design

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
+    <div className='w-full min-h-[40vh] p-5 mt-20 xl:px-20 xl:mt-28'>      
+        <div className="heading">
+            <h1 className='text-white font-montserrat text-center font-bold md:text-2xl'>About</h1>
+        </div>  
+        <div className="cards grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10">
+            <div className="card w-3/4 mx-auto min-h-[35vh] md:min-h-[20vh] lg:min-h-[45vh] xl:min-h-[30vh] flex flex-col justify-center  bg-black  rounded-md p-5 2xl:px-14 my-4">
+                <h1 className='text-primary fnt-bold font-montserrat xl:text-xl font-bold'>Free and <br /> Unlimited</h1>
+                <p className='text-white text-xs xl:text-sm xl:w-full mt-5 w-3/4 '>Convert and download as much as you like thousands of video/audio files for free.</p>
+            </div>
+            <div className="card w-3/4 mx-auto min-h-[35vh] md:min-h-[20vh] lg:min-h-[45vh] xl:min-h-[30vh] flex flex-col justify-center  bg-black  rounded-md p-5 2xl:px-14 my-4">
+                <h1 className='text-primary fnt-bold font-montserrat xl:text-xl font-bold'>No need to register <br /> an account</h1>
+                <p className='text-white text-xs xl:text-sm xl:w-full mt-5 w-3/4 '>Download quickly with no account registration and no need to install software and extensions</p>
+            </div>
+            <div className="card w-3/4 mx-auto min-h-[35vh] md:min-h-[20vh] lg:min-h-[45vh] xl:min-h-[30vh] flex flex-col justify-center  bg-black  rounded-md p-5 2xl:px-14 my-4">
+                <h1 className='text-primary fnt-bold font-montserrat xl:text-xl font-bold'>100% safe</h1>
+                <p className='text-white text-xs xl:text-sm xl:w-full mt-5 w-3/4 '>Does not collect and does not ask for any personal information. The downloaded file is safe and does not contain viruses.</p>
+            </div>
+            <div className="card w-3/4 mx-auto min-h-[35vh] md:min-h-[20vh] lg:min-h-[45vh] xl:min-h-[30vh] flex flex-col justify-center  bg-black  rounded-md p-5 2xl:px-14 my-4">
+                <h1 className='text-primary fnt-bold font-montserrat xl:text-xl font-bold'>Fast and flexible</h1>
+                <p className='text-white text-xs xl:text-sm xl:w-full mt-5 w-3/4 '>Super fast download and conversion speed. Flexible options of quality levels for downloadable video and audio files.</p>
+            </div>
+            <div className="card w-3/4 mx-auto min-h-[35vh] md:min-h-[20vh] lg:min-h-[45vh] xl:min-h-[30vh] flex flex-col justify-center  bg-black  rounded-md p-5 2xl:px-14 my-4">
+                <h1 className='text-primary fnt-bold font-montserrat xl:text-xl font-bold'>Supports all <br /> browsers and <br /> devices</h1>
+                <p className='text-white text-xs xl:text-sm xl:w-full mt-5 w-3/4 '>The website supports downloading on all browsers, such as Google Chrome, Firefox, Safari, Android Browser, Opera, UC Browser...and on all devices, including PC, Android, iPhone</p>
+            </div>
+            <div className="card w-3/4 mx-auto min-h-[35vh] md:min-h-[20vh] lg:min-h-[45vh] xl:min-h-[30vh] flex flex-col justify-center  bg-black   rounded-md p-5 2xl:px-14 my-4">
+                <h1 className='text-primary fnt-bold font-montserrat xl:text-xl font-bold'>Support high <br /> quality download</h1>
+                <p className='text-white text-xs xl:text-sm xl:w-full mt-5 w-3/4 '>Keep the quality of the downloaded file as the original video. Support HD download quality for video and 320kbps for MP3.</p>
+            </div>
+        </div>
       
     </div>
   )
--- src/Components/Mp3converter.js
+++ src/Components/Mp3converter.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Mp3converter() {
   return (
-    <div className='w-full min-h-[50vh]'>
+    <div className='w-full min-h-[30vh] '>
         <div className="heading text-center my-5 py-5 font-montserrat">
             <h1 className='text-primary text-lg md:text-2xl font-bold my-2 '>Mp3 <span className='text-white text-sm md:text-lg'> converter</span></h1>
             <p className='text-white text-[.5rem] md:text-xs my-2'>Convert and download Youtube videos in MP3 format for free.</p>
@@ -12,7 +12,7 @@ function Mp3converter() {
                 <input type="text" className='w-3/4 text-sm md:text-lg lg:text-base xl:text-lg bg-transparent focus:outline-none' placeholder='https://youtu.be/5hFd6zGkxLE'/>
                 <button className='text-black duration-300 active:scale-150'><i class="fa-solid fa-magnifying-glass md:text-xl"></i></button>
             </div>
-            <p className='text-[.5rem] text-white mt-5 md:text-xs md:mt-8'>By using our service you accept our Terms of Service</p>
+            <p className='text-[.5rem] text-white mt-5 md:text-xs md:mt-8 '>By using our service you accept our Terms of Service</p>
         </div>      
     </div>
   )
