a5cd79eeae3636ee152f57e8397565b0a961b41c
Muhammed Rashid <rashidktr07@gmail.com>
2023-01-11 13:40:18 +0530

    Starting Mp4 page Design

---
+++ src/Components/Mp4converter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mp4converter() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Mp4converter
--- src/Pages/YtConverterPage.js
+++ src/Pages/YtConverterPage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from '../Components/Header'
 import Mp3converter from '../Components/Mp3converter'
+import Mp4converter from '../Components/Mp4converter'
 
 function YtConverterPage() {
   return (
     <section className='w-full min-h-[100vh] bg-secondary'>
       <div className='container mx-auto'>
         <Header></Header>
-        <Mp3converter></Mp3converter>
+        {/* <Mp3converter></Mp3converter> */}
+        <Mp4converter></Mp4converter>
       </div>
     </section>
   )
