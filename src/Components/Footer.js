97c542552ddaf1fcca8ae3fb1f935a684d373899
Muhammed Rashid <rashidktr07@gmail.com>
2023-01-11 15:27:26 +0530

    complete Footer design

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      
+    <div className='w-full h-[10vh] mt-36 text-center'>
+        <h1 className='text-white font-montserrat text-xs'>@copyright 2001 and policies</h1>            
+        <a href='https://www.rapidapi.com' className='mt-4 text-white text-xs'>https://www.rapidapi.com</a>
     </div>
   )
 }
